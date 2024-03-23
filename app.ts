722d9a1b65c2751519d9b8490aa6a6418a8e3c1b
Taha khan <TahaNadeemkhan@example.com>
2024-03-23 12:05:38 -0700

    push my GuessMaster on GitHub!

---
+++ app.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random()*6+1);
+const numbers = await inquirer.prompt([{
+    name: "usersNumber",
+    type: "number",
+    message: "Enter the number between 1-6: ",
+},
+]);
+if (numbers.usersNumber === randomNumber) {
+    console.log("Congratulations, you guessed it right");
+} else{
+    console.log("Wrong Guess! Better luck next time.")
+}
